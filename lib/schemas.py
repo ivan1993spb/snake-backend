3e591677cd00f61f0290c1940621a72c8005e6ff
Ivan Pushkin <pushkin13@bk.ru>
2020-11-01 00:19:23 +0300

    Add colors to schemas

--- lib/schemas.py
+++ lib/schemas.py
@@ -1,16 +1,21 @@
 
 from enum import Enum
 from typing import List, Tuple, Union
+from abc import ABC, abstractmethod
 
-from pydantic import BaseModel
-from num2words import num2words
-
-
-# TODO: Use color from pydantic?
-# from pydantic.color import Color
+from pydantic import BaseModel, Field
 
 
 Dot = Tuple[int, int]
+
+ColorRGB = Tuple[int, int, int]
+
+ColorRGBApple: ColorRGB = (0x0, 0xff, 0x0)
+ColorRGBCorpse: ColorRGB = (0x0, 0x0, 0xff)
+ColorRGBMouse: ColorRGB = (0xff, 0x0, 0x0)
+ColorRGBSnake: ColorRGB = (0xff, 0x44, 0x44)
+ColorRGBWall: ColorRGB = (0x44, 0x77, 0x44)
+ColorRGBWatermelon: ColorRGB = (0xf0, 0xff, 0x0)
 
 
 class Game(BaseModel):
@@ -21,10 +26,6 @@ class Game(BaseModel):
     height: int
     rate: int
 
-    @property
-    def name(self) -> str:
-        return num2words(self.id)
-
     def is_full(self) -> bool:
         return self.count == self.limit
 
@@ -33,6 +34,18 @@ class Game(BaseModel):
 
     def is_empty(self) -> bool:
         return self.count == 0
+
+
+class OneDotObject(ABC):
+    """Any game object which consists of one dot
+    """
+    dot: Dot
+
+    @property
+    def dots(self) -> List[Dot]:
+        """List of dots
+        """
+        return [self.dot]
 
 
 class Games(BaseModel):
@@ -78,54 +91,69 @@ class ObjectType(str, Enum):
     watermelon = 'watermelon'
 
 
-class Apple(BaseModel):
+class Colored(ABC):
+    @staticmethod
+    @abstractmethod
+    def color() -> ColorRGB: return 0, 0, 0
+
+
+class Apple(BaseModel, Colored, OneDotObject):
     id: int
-    type = ObjectType.apple
+    type: ObjectType = Field(const=True, default=ObjectType.apple)
     dot: Dot
 
-    @property
-    def dots(self) -> List[Dot]: return [self.dot]
+    def color(self) -> ColorRGB: return ColorRGBApple
 
 
-class Corpse(BaseModel):
+class Corpse(BaseModel, Colored):
     id: int
-    type = ObjectType.corpse
+    type: ObjectType = Field(const=True, default=ObjectType.corpse)
     dots: List[Dot]
 
+    @staticmethod
+    def color() -> ColorRGB: return ColorRGBCorpse
 
-class Mouse(BaseModel):
+
+class Mouse(BaseModel, Colored, OneDotObject):
     id: int
-    type = ObjectType.mouse
+    type: ObjectType = Field(const=True, default=ObjectType.mouse)
     dot: Dot
     direction: str
 
-    @property
-    def dots(self) -> List[Dot]: return [self.dot]
+    @staticmethod
+    def color() -> ColorRGB: return ColorRGBMouse
 
 
-class Snake(BaseModel):
+class Snake(BaseModel, Colored):
     id: int
-    type = ObjectType.snake
+    type: ObjectType = Field(const=True, default=ObjectType.snake)
     dots: List[Dot]
 
+    @staticmethod
+    def color() -> ColorRGB: return ColorRGBSnake
 
-class Wall(BaseModel):
+
+class Wall(BaseModel, Colored):
     id: int
-    type = ObjectType.wall
+    type: ObjectType = Field(const=True, default=ObjectType.wall)
     dots: List[Dot]
 
+    @staticmethod
+    def color() -> ColorRGB: return ColorRGBWall
 
-class Watermelon(BaseModel):
+
+class Watermelon(BaseModel, Colored):
     id: int
-    type = ObjectType.watermelon
+    type: ObjectType = Field(const=True, default=ObjectType.watermelon)
     dots: List[Dot]
+
+    @staticmethod
+    def color() -> ColorRGB: return ColorRGBWatermelon
 
 
 AnyObject = Union[Apple, Corpse, Mouse, Snake, Wall, Watermelon]
 
-# TODO: Define the proper type
-# AnyObjectList = List[AnyObject]
-AnyObjectList = List[dict]
+AnyObjectList = List[AnyObject]
 
 
 class Objects(BaseModel):
